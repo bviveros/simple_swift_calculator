3b7ef30ba3e5a7bbd8909832fcbf2ab11ffb0a9f
Braulio Viveros <bobbyviveros9@gmail.com>
2024-05-12 19:26:45 -0600

    complete overhaul of the project

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-# Temporary Functionality
-Converts text into ASCII text
-
-## How to use
-swift run MyCLI --input \<input text in single quotes\>
-
 --- 
 
 # simple swift calculator (Eventually)
---
+++ add_subtract_calc/add_subtract_calcApp.swift
@@ -0,0 +1,17 @@
+//
+//  add_subtract_calcApp.swift
+//  add_subtract_calc
+//
+//  Created by Braulio Viveros on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct add_subtract_calcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
